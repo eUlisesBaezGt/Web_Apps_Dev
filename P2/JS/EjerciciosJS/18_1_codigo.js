0558651e5c042086d36fc949d73d2234235e9768
kikinacademy <kikinacademy@gmail.com>
2022-09-21 00:28:26 -0500

    JS

--- P2/JS/EjerciciosJS/18_1_codigo.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-    console.log("Hola Mundo!"); {/* Imprime en la consola del navegador */}
-    {/* Después del primer mensaje, se debe mostrar otro mensaje que diga "Soy el primer script" */}
-    console.log("Soy el primer script");
-    {/* Añadir en la página XHTML un mensaje de aviso para los navegadores que no tengan activado el soporte de JavaScript */}
-    console.log("Este sitio web requiere JavaScript para funcionar correctamente");
-
-    </script>
